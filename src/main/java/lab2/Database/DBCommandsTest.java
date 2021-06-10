b65876e37fdcf107cdd988335c1dbf435d801cd0
Valeriia <valeriia.bykovets@ukma.edu.ua>
2021-06-10 23:42:04 +0300

    Tests

--- src/main/java/lab2/Database/DBCommandsTest.java
+++ src/main/java/lab2/Database/DBCommandsTest.java
@@ -2,6 +2,8 @@ package lab2.Database;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DBCommandsTest {
@@ -25,4 +27,32 @@ class DBCommandsTest {
         assertEquals(2,comm.Read().size() );
         comm.DeleteAll();
     }
+    @Test
+    void testCriteria(){
+        DBCommands comm=new DBCommands();
+        comm.Create("Test3");
+        comm.DeleteAll();
+       comm.Insert(new Product("pineapple", 45, 86));
+        comm.Insert(new Product("meat", 123, 23));
+        comm.Insert(new Product("meat", 245, 3));
+        comm.Insert(new Product("milk", 23, 90));
+        List<Product> res=comm.listByName("meat");
+        assertEquals(2, res.size() );
+        comm.DeleteAll();
+    }
+    @Test
+    void testCriteriaPrice(){
+        DBCommands comm=new DBCommands();
+        comm.Create("Test3");
+        comm.DeleteAll();
+        comm.Insert(new Product("pineapple", 45, 86));
+        comm.Insert(new Product("meat", 123, 23));
+        comm.Insert(new Product("meat", 245, 3));
+        comm.Insert(new Product("milk", 23, 90));
+        comm.Insert(new Product("juice", 10, 23));
+        List<Product> res=comm.listByPrice(">24");
+        assertEquals(3, res.size() );
+        comm.DeleteAll();
+    }
+
 }
