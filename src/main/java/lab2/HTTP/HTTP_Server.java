fc19b145e4b36fed6bbf4a01b99d7b488cc0faa1
YevheniiKyr <jekafekz@gmail.com>
2021-06-19 21:41:57 +0300

    jwt/user/login~

--- src/main/java/lab2/HTTP/HTTP_Server.java
+++ src/main/java/lab2/HTTP/HTTP_Server.java
@@ -2,15 +2,15 @@ package lab2.HTTP;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.security.Key;
+
+import com.sun.net.httpserver.*;
 import io.jsonwebtoken.*;
 import java.util.Date;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
 import lab2.Database.DBCommands;
+import lab2.Database.Product;
 import lab2.Database.User;
 
 import java.io.IOException;
@@ -21,6 +21,9 @@ import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 public class HTTP_Server {
+
+
+
     static Key signingKey;
     public static int getPort() {
         return port;
@@ -33,7 +36,7 @@ public class HTTP_Server {
     private static int port = 1234;
     public static void main(String [] args) throws IOException   {
         DBCommands comm = new DBCommands();
-        comm.Create();;
+        comm.Create();
        // comm.InsertUser(new User("login3","password"));
        // comm.InsertUser(new User("login4","password"));
         String jwt = createJWT("login");
@@ -61,7 +64,6 @@ public class HTTP_Server {
         });
         server.createContext("/login",exchange -> {
             if(exchange.getRequestMethod().equals("POST")){
-
                // byte[] response = "{\"status\": \"ok\"}".getBytes(StandardCharsets.UTF_8);
                 byte[] response = "{\"login\": \"ok\",\"password\" : \"password\" }".getBytes(StandardCharsets.UTF_8);
                User user =  myMapper.readValue(response, User.class);
@@ -72,8 +74,7 @@ public class HTTP_Server {
                       exchange.getResponseHeaders().set("Authorization", createJWT(find.getLogin()));
                       exchange.sendResponseHeaders(200,0);
                    //   String jwt = createJWT(find.getLogin());
-                    //  System.out.println(findUserByJWT(jwt));
-
+                     //System.out.println(findUserByJWT(jwt));
                      }
                      else   exchange.sendResponseHeaders(401,0);
 
@@ -82,16 +83,59 @@ public class HTTP_Server {
 
                         exchange.sendResponseHeaders(401,0);
                     }
-
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
-
-
             }
             else{
                 exchange.sendResponseHeaders(405, 0);
+            }
+        });
 
+
+        HttpContext context = server.createContext("/api/good", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                System.out.println(exchange.getPrincipal());
+                if(exchange.getRequestMethod().equals("GET")) {
+
+                    Product prod = new Product("milk",10,10);
+
+                    byte[] response = myMapper.writeValueAsBytes(prod);
+                    exchange.getResponseHeaders().set("Content-Type", "application/json");
+                    exchange.sendResponseHeaders(200, response.length);
+                    exchange.getResponseBody().write(response);
+                    exchange.close();
+                }
+                else{
+                    exchange.sendResponseHeaders(404, 0);
+                }
+            }
+        });
+        context.setAuthenticator(new Authenticator() {
+            @Override
+            public Result authenticate(HttpExchange exch) {
+               String jwt =  exch.getRequestHeaders().getFirst("Authorization");
+               if(jwt!=null){
+
+                   String login = findUserByJWT(jwt);
+                   try {
+                       User user =  comm.getUserByLogin(login);
+                       if(user!=null){
+                       return new Success(new HttpPrincipal(login,"user"));
+
+                       }
+
+                           return new Failure(403);
+                   } catch (SQLException throwables) {
+                       throwables.printStackTrace();
+                   }
+
+
+               }
+               System.out.println("kk");
+             //   return new Failure(403);
+                return null;
             }
         });
 
