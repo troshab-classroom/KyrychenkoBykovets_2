f9bd4fb040a081e7af78b4afa888b707e9669512
YevheniiKyr <jekafekz@gmail.com>
2021-06-19 19:19:38 +0300

    jwt/user/login

--- src/main/java/lab2/HTTP/HTTP_Server.java
+++ src/main/java/lab2/HTTP/HTTP_Server.java
@@ -1,15 +1,27 @@
 package lab2.HTTP;
+import javax.crypto.spec.SecretKeySpec;
 
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import lab2.Database.DBCommands;
+import lab2.Database.User;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 
 public class HTTP_Server {
+    static Key signingKey;
     public static int getPort() {
         return port;
     }
@@ -20,6 +32,16 @@ public class HTTP_Server {
 
     private static int port = 1234;
     public static void main(String [] args) throws IOException   {
+        DBCommands comm = new DBCommands();
+        comm.Create();;
+       // comm.InsertUser(new User("login3","password"));
+       // comm.InsertUser(new User("login4","password"));
+        String jwt = createJWT("login");
+        System.out.println(findUserByJWT(jwt));
+        ObjectMapper myMapper = new ObjectMapper();
+        ///byte[] response = "{\"login\": \"ok\",\"password\" : \"password\" }".getBytes(StandardCharsets.UTF_8);
+        //User user = myMapper.readValue(response, User.class);
+       // System.out.println(user);
         HttpServer server = HttpServer.create(new InetSocketAddress(port),0);
         server.start();
         server.createContext("/", new HttpHandler() {
@@ -33,16 +55,81 @@ public class HTTP_Server {
                     exchange.close();
                 }
                 else{
-
                     exchange.sendResponseHeaders(405, 0);
+                }
+            }
+        });
+        server.createContext("/login",exchange -> {
+            if(exchange.getRequestMethod().equals("POST")){
 
+               // byte[] response = "{\"status\": \"ok\"}".getBytes(StandardCharsets.UTF_8);
+                byte[] response = "{\"login\": \"ok\",\"password\" : \"password\" }".getBytes(StandardCharsets.UTF_8);
+               User user =  myMapper.readValue(response, User.class);
+                try {
+                    User find = comm.getUserByLogin(user.getLogin());
+                    if(find!=null){
+                     if (find.getPassword().equals(user.getPassword())){
+                      exchange.getResponseHeaders().set("Authorization", createJWT(find.getLogin()));
+                      exchange.sendResponseHeaders(200,0);
+                   //   String jwt = createJWT(find.getLogin());
+                    //  System.out.println(findUserByJWT(jwt));
 
+                     }
+                     else   exchange.sendResponseHeaders(401,0);
+
+                    }
+                    else{
+
+                        exchange.sendResponseHeaders(401,0);
+                    }
+
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
                 }
 
 
-             //   exchange.sendResponseHeaders();
+            }
+            else{
+                exchange.sendResponseHeaders(405, 0);
+
             }
         });
 
     }
+
+
+    private static String createJWT(String login) {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        byte[] apiKeySecretBytes = "secretlongbesthowruimfinetahks".getBytes();
+        signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+
+        JwtBuilder builder = Jwts.builder()
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+ TimeUnit.HOURS.toMillis(14)))
+                .setSubject(login)
+                .signWith(signatureAlgorithm, signingKey);
+
+
+
+        return builder.compact();
+    }
+    private static String  findUserByJWT(String jwt) {
+
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        Claims claims = Jwts.parser()
+                .setSigningKey(signingKey)
+                .parseClaimsJws(jwt).getBody();
+      //  System.out.println("ID: " + claims.getId());
+     //   System.out.println("Subject: " + claims.getSubject());
+     //   System.out.println("Issuer: " + claims.getIssuer());
+       // System.out.println("Expiration: " + claims.getExpiration());
+        return claims.getSubject();
+    }
+
     }
